be5b573201ab6401823416dbab912c6f95fceb44
Anhyeonggwan <anhyeonggwan@gmail.com>
2025-04-16 17:05:44 +0900

    first commit

---
+++ src/main/java/com/aws/awsarticle/AwsarticleApplication.java
@@ -0,0 +1,13 @@
+package com.aws.awsarticle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsarticleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsarticleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aws/awsarticle/AwsarticleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.awsarticle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsarticleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
